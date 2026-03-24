52b818e12883e0e2274f030cb5ed230f7f80951a
hokuessentials <saifinaushad20@gmail.com>
2026-03-25 01:45:01 +0530

    Update atlas_gateway.py

--- atlas_gateway.py
+++ atlas_gateway.py
@@ -11,7 +11,8 @@ import datetime
 # 2. APP INITIALIZATION
 # ================================
 app = Flask(__name__)
-APPS_SCRIPT_URL = os.environ.get("APPS_SCRIPT_URL")
+load_session_from_sheet()
+APPS_SCRIPT_URL = os.environ.get("https://script.google.com/macros/s/AKfycbzE0aSjAWHgONC-GT4hFlMmq830hkMWsKR96Hla2yxOgzLhcPtNH-Ua3Llqjz9GAh5Xkg/exec")
 
 # ================================
 # 3. GLOBAL STATE
